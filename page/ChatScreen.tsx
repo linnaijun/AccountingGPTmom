1e053df6f6987ae5c6d9f83e6fc551f922df98d1
Peggy <peggy4328@gmail.com>
2023-11-30 16:08:16 +0800

    layout v2

--- page/ChatScreen.tsx
+++ page/ChatScreen.tsx
@@ -3,11 +3,11 @@ import { StyleSheet, View, Text, ScrollView, Image } from 'react-native';
 import RNFS from 'react-native-fs';
 import { useFocusEffect } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 interface Message {
   key: number;
   text: string;
   speaker: string;
+  time: number;
 }
 
 const ChatScreen = () => {
@@ -28,14 +28,16 @@ const ChatScreen = () => {
             loadedMessages.push({
               key: index * 2, 
               text: data.usertalk,
-              speaker: 'User'
+              speaker: 'User',
+              time: data.time.split('T')[0]
             });
           }
           if (data.talk) {
             loadedMessages.push({
               key: index * 2 + 1, 
               text: data.talk, 
-              speaker: 'GPT'
+              speaker: 'GPT',
+              time: data.time.split('T')[0]
             });
           }
         }
@@ -59,7 +61,7 @@ const ChatScreen = () => {
   }, [messages]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
       <View style={styles.title}>
         <Icon name="event-note" size={24} color="#000" style={styles.icon1} />
         <Text style={styles.title_text}>
@@ -67,24 +69,48 @@ const ChatScreen = () => {
         </Text>
         <Icon name="search" size={24} color="#000" style={styles.icon2} />
       </View>
-      <ScrollView ref={scrollViewRef} style={{ flex: 1, width: '100%' }}>
+      <ScrollView ref={scrollViewRef} style={styles.scroll}>
         {messages.map((message) => (
           <View key={message.key} style={{
             alignSelf: message.speaker === 'GPT' ? 'flex-start' : 'flex-end',
             backgroundColor: message.speaker === 'GPT' ? '#fffbe2' : '#d2e0fb',
-            margin: 10,
-            padding: 10,
-            borderRadius: 5,
-            maxWidth: '66%'
+            marginLeft: message.speaker === 'GPT' ? 20 : 10,
+            marginRight: message.speaker === 'GPT' ? 10 : 20,
+            margin: 10,         
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 15,
+            paddingRight: 15,
+            borderRadius: 7,
+            maxWidth: '66%',
+            flexDirection: 'row',
           }}>
-            {message.speaker === 'GPT' ? `<Image style={styles.role} source = {require('../img/cat_head.png')} />` : ''}
-            <Text>{message.text}</Text>
+            {message.speaker === 'GPT' && (
+              <Image style={styles.messageLeft} source={require('../img/messageLeft.png')} />
+            )}
+            {message.speaker !== 'GPT' && (
+              <Image style={styles.messageRight} source={require('../img/messageRight.png')} />
+            )}
+            {message.speaker === 'GPT' && (
+              <Image style={styles.role} source={require('../img/cat_head.png')} />
+            )}
+            <Text style={{
+              paddingRight: message.speaker === 'GPT' ? 15 : '',
+              marginLeft: message.speaker === 'GPT' ? 10 : '',
+              color: '#49454f',
+              fontSize: 14,
+              fontFamily: 'Roboto',
+            }}>
+              {message.text}
+            </Text>
           </View>
         ))}
       </ScrollView>
     </View>
   );
 };
+
+
 
 const styles = StyleSheet.create({
   title: {
@@ -94,6 +120,8 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor:'#fffbe2',
+    elevation: 2,
+    zIndex: 1,
   },
   title_text: {
     fontSize: 22,
@@ -107,7 +135,30 @@ const styles = StyleSheet.create({
   icon2: {
     paddingTop: 10,
     paddingRight: 16
-  }
+  },
+  scroll: {
+    flex: 1, 
+    width: '100%',
+  },
+  role: {
+    height: 20,
+    width: 20,
+    marginTop: 1,
+  },
+  messageLeft: {
+    height: 10,
+    width: 15,
+    position: 'absolute',
+    left: -8,
+    bottom: -3
+  },
+  messageRight: {
+    height: 10,
+    width: 15,
+    position: 'absolute',
+    right: -8,
+    bottom: -3
+  },
 });
 
 export default ChatScreen;
